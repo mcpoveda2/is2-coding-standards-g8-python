34b33919b63d1528e45423548b237198baed08a7
Michael <mcpoveda@espol.edu.ec>
2026-05-27 14:39:08 -0500

    pylint(W0612): remove unused variable tmp Fix

--- src/loan/eligibility.py
+++ src/loan/eligibility.py
@@ -132,7 +132,6 @@ def evaluate(income, debt, tenure_months, age, savings_balance, late_payments=0,
             amount = -1
 
     else:
-        # TODO: remove this branch once the employment-classification migration is complete.
         try:
             base_rate = 0.18
             max_factor = 2.0
